888d2b95a75ca63859615ba9279bd6718a958ac0
MFZL24 <mifzalzal9@gmail.com>
2024-04-12 09:12:28 +0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Open and close the search form.
    */
-  const searchOpen = document.querySelector(".js-search-open");
+  /*const searchOpen = document.querySelector(".js-search-open");
   const searchClose = document.querySelector(".js-search-close");
   const searchWrap = document.querySelector(".js-search-form-wrap");
 
